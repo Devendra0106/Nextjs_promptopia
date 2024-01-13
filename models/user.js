df015eae17809e1e64a5d334eb19d97b6f747e55
Devendra <devendramorankar@gmail.com>
2024-01-13 23:54:02 +0530

    added comments for model

--- models/user.js
+++ models/user.js
@@ -19,5 +19,12 @@ const UserSchema = new Schema({
 	},
 });
 
+// The "models" object is provided by the Mongoose library and stores all the registered models.
+// If a model named "User" already exists in the "models" object, it assigns that existing model to the "User" variable.
+// This prevents redefining the model and ensures that the existing model is reused.
+
+// If a model named "User" does not exist in the "models" object, the "model" function from Mongoose is called to create a new model.
+// The newly created model is then assigned to the "User" variable.
+
 const User = models.User || model("User", UserSchema);
 export default User;
